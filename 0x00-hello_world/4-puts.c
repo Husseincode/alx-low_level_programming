a1eb2941f70ef3783af1f93d19616f165578f1d1
Hussein <akanjiabayomi2@gmail.com>
2023-08-10 09:33:54 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A function that prints an output with put.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
