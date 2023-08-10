225e8269b34fefb6f68c889025bdd018e70019fd
Hussein <akanjiabayomi2@gmail.com>
2023-08-10 10:30:04 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Main - A program that prints sizes of various types
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+	float decimal;
+	char letter;
+	printf("The size of an integer is %d \n The size of float is %f \n The size of char is %s", num, decimal, letter)
