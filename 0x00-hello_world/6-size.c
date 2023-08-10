7314280d5459cc590b82e744bb1449bb6719008b
Hussein <akanjiabayomi2@gmail.com>
2023-08-10 11:01:04 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,9 @@ int main(void)
 	int num;
 	float decimal;
 	char letter;
-	printf("The size of an integer is %ld \n The size of float is %ld \n The size of char is %ld", sizeof(num), sizeof(decimal), sizeof(letter));
+	long int c;
+	long long int d;
+	printf("Size of a char: %ld byte(s) \n Size of an int: %ld \n Size of a long int: %ld \n Size of a long long int: %ld \n Size of a float: %ld", sizeof(letter), sizeof(num), sizeof(c),sizeof(d), sizeof(decimal));
 	return 0;
 }
+
