4d2bd629fdc7dbaf17ab6387bea8728b3ab8e28d
Hussein <akanjiabayomi2@gmail.com>
2023-08-11 04:38:46 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
+/**
+ * main - A program that shows the conditional statement between numbers
+ * Return: 0 (Success)
+ */
+
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -10,15 +15,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there*/
-	if(n > 0)
+
+	if (n > 0)
 	{
 		printf("is positive");
 	}
-	else if(n == 0)
+
+	else if (n == 0)
 	{
 		printf("zero");
 	}
-	else if(n < 0)
+
+	else if (n < 0)
 	{
 		printf("is negative");
 	}
