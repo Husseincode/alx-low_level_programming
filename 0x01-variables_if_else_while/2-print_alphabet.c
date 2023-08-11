105f20deacf036529912487f1af13ca41ef02d80
Hussein <akanjiabayomi2@gmail.com>
2023-08-11 13:05:20 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,7 +6,7 @@
 
 int main(void)
 {
-	char alphabets[26] = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+	char alphabets[29] = "abcdefghiklmnopqrstuvwxyz";
 	int counter;
 
 	for (counter = 0; counter < 26; counter++)
