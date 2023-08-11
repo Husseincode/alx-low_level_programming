5c3793f0d5a3deff5d9c1d4259ba774ca5dcab5d
Hussein <akanjiabayomi2@gmail.com>
2023-08-11 14:29:52 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - The program that prints lowercase except q and e
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int a;
+
+	for (a = 0; a < 26; a++)
+	{
+		if (alp[a] == 'q' || alp[a] == 'e')
+		{
+			putchar(alp[a]);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
