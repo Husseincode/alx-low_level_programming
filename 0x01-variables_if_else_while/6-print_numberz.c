9bd825e60ee2874e99380cf159d8089930cfcf62
Hussein <akanjiabayomi2@gmail.com>
2023-08-12 13:03:48 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all single digit numbers
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
