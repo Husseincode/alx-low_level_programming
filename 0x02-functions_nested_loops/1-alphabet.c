e7e1956aff5a8491dc3863fc3c7dfc13a49ef66c
Hussein <akanjiabayomi2@gmail.com>
2023-08-15 13:30:51 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - A function that prints the alphabet, in lowercase, followed by a new line.
+ * Return: 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char alp;
+
+	for (alp = 'a' alp <= 'z' alp++)
+	{
+		printf(alp);
+	}
+	return (0);
+}
