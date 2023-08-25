ee64db8225f00927da4bd4322160a1a8b1b2c232
Hussein <akanjiabayomi2@gmail.com>
2023-08-25 09:29:14 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,7 +7,7 @@
 
 void print_most_numbers(void)
 {
-	int numbers[15] = {'0', '1', '3', '5', '6', '7', '8', '9', '\0'};
+	int numbers[10] = {'0', '1', '3', '5', '6', '7', '8', '9', '\0'};
 	int i;
 
 	for (i = 0; i < 10; i++)
