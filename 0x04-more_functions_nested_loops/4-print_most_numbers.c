8e9266a680bfc2a83cd3ef16e701f7397ea0efef
Hussein <akanjiabayomi2@gmail.com>
2023-08-25 09:25:26 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - a function that prints the numbers.
+ * Return: 0(success)
+ */
+
+void print_most_numbers(void)
+{
+	int numbers[15] = {'0', '1', '3', '5', '6', '7', '8', '9', '\0'};
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(numbers[i]);
+	}
+	_putchar('\n');
+}
