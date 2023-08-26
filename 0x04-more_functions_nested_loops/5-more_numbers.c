4da3de3eab7aa766cc80498437cb7d7db2c9b5a2
Hussein <akanjiabayomi2@gmail.com>
2023-08-26 05:29:36 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - a function that prints 10 times the numbers.
+ * Return: 0 (Success)
+ */
+
+void more_numbers(void)
+{
+	char numbers[20] = "01234567891011121314";
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			_putchar(numbers[j]);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
